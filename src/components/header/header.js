e8968bf1aed923e27c4877b113f78d40802ea2d2
Ashrithanch09 <151167244+Ashrithanch09@users.noreply.github.com>
2024-04-07 18:46:24 +0530

    Create header.js

---
+++ src/components/header/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  
+  return (
+    <header>
+	<nav>
+    	<ul>
+       	<li><a href="/home"><img src="images/home-icon.png" alt="Home"></a></li>
+       	<li><a href="/products"><img src="images/products-icon.png" alt="Products"></a></li>
+    	</ul>
+	</nav>
+ 
+    </header>
+  );
+};
+
+export default Header;
