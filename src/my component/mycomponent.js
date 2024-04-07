1f632c7455e81c3303054bdc43152951b11ca57f
Ashrithanch09 <151167244+Ashrithanch09@users.noreply.github.com>
2024-04-07 19:06:18 +0530

    Update mycomponent.js

--- src/my component/mycomponent.js
+++ src/my component/mycomponent.js
@@ -5,14 +5,14 @@ const MyComponent = () => {
   return (
     <article>
       <Helmet>
-        {/* Meta tags as shown previously */}
+       
       </Helmet>
-      <h1>Main Title of the Page</h1>
+      <h1>DISCOVER OUR PRODUCTS</h1>
       <section>
         <h2>Subsection Title</h2>
-        {/* Content */}
+       
       </section>
-      {/* More content */}
+      
     </article>
   );
 };
