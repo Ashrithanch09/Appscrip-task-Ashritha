3d32958682ecb24a586920edc9a7b8cacbf9188e
Ashrithanch09 <151167244+Ashrithanch09@users.noreply.github.com>
2024-04-07 18:59:57 +0530

    Create home.js

---
+++ src/components/layout/home.js
@@ -0,0 +1,13 @@
+
+import Layout from '../components/Layout';
+
+const Home = () => {
+    return (
+        <Layout title="Home Page">
+            <h1>Welcome to my website!</h1>
+            
+        </Layout>
+    );
+};
+
+export default Home;
